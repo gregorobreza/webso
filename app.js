761247c9ae4e4fb1adaf74c2bd6a37890558b8af
Jure <jurespeh10@gmail.com>
2020-09-07 22:06:10 +0200

    dodana sekcija o nas

--- app.js
+++ app.js
@@ -94,3 +94,55 @@ function showSlides(n) {
   captionText[slideIndex - 1].style.display = "block";
   
 }
+
+/* Animacija tipkanja */
+
+const textTyping = ['študent elektrotehnike.', 'športni navdušenec.', 'ljubitelj knjig.', 'reševalec težav' ];
+
+let count = 0;
+
+let index = 0;
+
+let currentText = '';
+
+let letter = '';
+
+
+(function type(){
+  if( count === textTyping.length){
+    count = 0;
+  }
+  currentText = textTyping[count];
+  letter = currentText.slice(0, ++index);
+  document.querySelector('.typing').textContent = letter;
+  
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 300);
+
+}());
+
+
+const textTyping1 = ['študent strojništva', 'navdušen programer.', 'powerlifter.', 'mojster za vse'];
+let count1 = 0;
+let index1 = 0;
+let currentText1 = '';
+let letter1 = '';
+
+(function type1(){
+  if( count1 === textTyping1.length){
+    count1 = 0;
+  }
+  currentText1 = textTyping1[count1];
+  letter1 = currentText1.slice(0, ++index1);
+  document.querySelector('.typing1').textContent = letter1;
+
+  if(letter1.length === currentText1.length){
+    count1++;
+    index1 = 0;
+  }
+  setTimeout(type1, 300);
+
+}());
